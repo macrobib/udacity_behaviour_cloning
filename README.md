f0d1646748a0425b0275fa10c5c644fb072cb626
macrobib <bibinc17@gmail.com>
2017-07-09 09:21:45 +0800

    Cleanup and notebook updates

--- README.md
+++ README.md
@@ -17,37 +17,39 @@ python drive.py model.h5
 
 ##### Model Architecture:
 Two different models were looked into during the implementation phase, they were [comma.ai](https://arxiv.org/abs/1608.01230) model and [Nvidia Model](https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf)   
-As part of final implementation a modified version of nvidia model was used. The nvidia model is shown below.
-
-![Nvidia model](./imgs/nvidia.JPG)
+As part of final implementation a modified version of nvidia model was used. The architecture of final trained model is shown below.
+![Trained model](./model.png)
+- In windows due to the [issue](https://stackoverflow.com/questions/41847376/keras-model-to-json-error-rawunicodeescape-codec-cant-decode-bytes-in-posi) with unicode parsing in lambda layer of tensorflow, instead of adding normalization layer, each image is first normalized as part of the batch generator in _imageHandler.py: process_batch()_.
+- Batch normalization is used after Convolution layers and Dense layers.
 - RELU Activation was used as activation layer.
 - Max pooling was done after each convolutional layer.
 - 'same' image padding was used instead of 'valid'.
-- As an attempt to reduce over fitting dropout of 0.28 was added before the first fully connected layer.
-These changes were done based on driving accuracy on the simulator and was a trial and error approach.
+- As an attempt to reduce over fitting dropout of 0.2 is added after convolution layers and a dropout of 0.5 is added after dense layers.
+   -These changes were done based on driving accuracy on the simulator and was a trial and error approach.
 - Though Adam optimizer was used for optimization, it was set to an initial training rate of 0.0001 instead of default.
 - An 80: 20 split of randomly shuffle data was used to train and validate image.
-- Different epoch were tried during the training: 8, 10, 12, 15 and 8 was finally choosen for best performance of all.
+- Initially training was done with an epoch of 30 with an EarlyStopping callback registered. Though validation error decreased till 30 epochs, the model was slow in reacting to corners in simulator and hence epoch was reduced to 20.
 
 ##### Data collection strategy.
 - The quality of the collected data had significant impact on the driving accuracy in this project.
 - An initial attempt was made to collect extensive data in straight and driving in opposite direction in the track-1. But the model failed on bridge corners.
-In the final approach following were the collected data:
+  In the final approach following were the collected data:
 - straight-recovery: 20313(including left, center and right camera) images of slow recovery from side.  
-![straight recovery](./imgs/center_2017_04_21_17_35_49_845.jpg)
+  ![straight recovery](./imgs/center_2017_04_21_17_35_49_845.jpg)
 - wiggle : 23655 images of fast course correction between the lane lines.  
-![wiggle](./imgs/center_2017_04_22_23_35_45_301.jpg)
+  ![wiggle](./imgs/center_2017_04_22_23_35_45_301.jpg)
 - recover-unpatched : 2244 images of recovering near special cases of track, including curve after the bridge.  
-![unpatched](imgs/center_2017_04_22_23_57_33_281.jpg)
+  ![unpatched](imgs/center_2017_04_22_23_57_33_281.jpg)
 - center : 20976 image of driving with vehicle on center of the track.  
-![center](./imgs/center_2017_04_23_03_24_11_543.jpg)
+  ![center](./imgs/center_2017_04_23_03_24_11_543.jpg)
 - Data collection was made with speed in and around 20, also throttle was limited to keep the trained model within speed range of 20.
 
 ##### Reducing overfitting:
-- Dropout was introduced to reduce overfitting.
+- Dropout was introduced to reduce overfitting for both convolution and dense layers.
+- EarlyStopping callback was registered to stop training in case validation accuracy failing to improve.
 - Images were randomly perturbed, images were sheared or flipped randomly to better generalize the model.
 - Angle correction were added to the left and right camera images to help in steering recovery.
-
+- 
 ##### Conclusion:
 - A recorded [video](./video.mp4) of the model is part of this submission.
 - The model was extremely susceptible to the type of data collected, as such a much more generalized model using transfer learning, from model trained on actual road scenes need to be tried.
--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout, Activation, MaxPooling2D, ELU, BatchNormalization
 from keras.layers.convolutional import Convolution2D
 from keras.models import Sequential
@@ -7,24 +6,17 @@ from keras import backend as K
 import matplotlib.pyplot as plt
 from keras.utils.visualize_util import plot
 from keras.callbacks import EarlyStopping
-
-
 from imageHandler import imageHandler
 
 epoch_count = 10
 learn_rate = 0.0001
 data_folders = [
-                # "./data/straight-bridge",
                 "./data/bridge-turn",
-                # "./data/track-3",
-                # "./data/straight-corners",
                 "./data/straight-recovery",
                 "./data/center",
                 "./data/recover-unpatched",
-                "./data/wiggle",#,
-                # "./data/straight-reverse"]#,
-                "./data/straight-smooth"]#,
-                 # "./data/straight-recovery-side"]
+                "./data/wiggle",
+                "./data/straight-smooth"]
 
 imgH = imageHandler(data_folders)
 data_count = imgH.get_data_count()
@@ -43,7 +35,6 @@ def plot_history(history):
 def comma_model():
     model = Sequential()
     model.add(BatchNormalization(input_shape=(160, 320, 3)))
-    # model.add(Convolution2D(16, 8, 8,input_shape=(160, 320, 3), subsample=(4, 4), border_mode="same"))
     model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
     model.add(ELU())
     model.add(Dropout(.5))
@@ -63,43 +54,37 @@ def comma_model():
     model.compile(optimizer=Adam(lr=0.0001), loss="mse")
     return model
 
-
 def nvidia_model():
     model = Sequential()
-
-    # model.add(BatchNormalization(input_shape=(66, 200, 3)))
-    # model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
-    model.add(Convolution2D(24, 5, 5, input_shape=(66, 200, 3), border_mode='same'))
+    model.add(Convolution2D(24, 5, 5, input_shape=(66, 200, 3), border_mode='same', subsample=(2, 2)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    # model.add(Dropout(0.2))
+    model.add(Dropout(0.2))
 
     model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    # model.add(Dropout(0.2))
+    model.add(Dropout(0.2))
 
     model.add(Convolution2D(48, 3, 3, border_mode='same', subsample=(2, 2)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    # model.add(Dropout(0.4))
+    model.add(Dropout(0.2))
 
     model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    # model.add(Dropout(0.4))
+    model.add(Dropout(0.2))
 
     model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
-    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
     model.add(Flatten())
-    model.add(Dropout(0.5))
-
     model.add(Dense(1164))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
@@ -113,70 +98,10 @@ def nvidia_model():
     model.add(Dense(50))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    # model.add(Dropout(0.2))
 
     model.add(Dense(10))
-    model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Dense(1))
-    model.compile(optimizer=Adam(learn_rate), loss="mse")
-    model.summary()
-    return model
 
-
-def nvidia_model_2():
-    model = Sequential()
-    # model.add(BatchNormalization(input_shape=(66, 200, 3)))
-    # model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
-    model.add(Convolution2D(24, 5, 5, input_shape=(66, 200, 3), border_mode='same',
-                            subsample=(2, 2), kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    model.add(Dropout(0.5))
-
-    model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2),
-                            kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    model.add(Dropout(0.4))
-
-    model.add(Convolution2D(48, 3, 3, border_mode='same', subsample=(2, 2),
-                            kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    model.add(Dropout(0.4))
-
-    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1),
-                            kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    model.add(Dropout(0.4))
-
-    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1),
-                            kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(Activation('relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-    #
-    model.add(Flatten())
-    model.add(Dense(1164, kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(50, kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    # model.add(Dropout(0.2))
-    model.add(Dense(10, kernel_initializer='he_normal', bias_initializer='zeros'))
-    model.add(Activation('relu'))
     model.add(Dense(1))
     model.compile(optimizer=Adam(learn_rate), loss="mse")
     model.summary()
@@ -187,7 +112,7 @@ model = nvidia_model()
 early_stopping = EarlyStopping(monitor='val_loss', patience=2, min_delta=0.0002, mode='min')
 train_samples = data_count[0] - data_count[0]%128
 val_samples = data_count[1] - data_count[1]%128
-hist = model.fit_generator(generator=imgH.get_training_batch(), samples_per_epoch=train_samples, nb_epoch=20,
+hist = model.fit_generator(generator=imgH.get_training_batch(), samples_per_epoch=train_samples, nb_epoch=12,
                                      validation_data=imgH.get_validation_batch(), nb_val_samples=val_samples, callbacks=[early_stopping])
 
 plot(model, to_file='model.png', show_shapes=True)
